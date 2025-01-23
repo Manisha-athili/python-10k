50b085eba62b0a5fd209100014fc9c3f20cb7737
Manisha-athili <manishaathili1126@gmail.com>
2025-01-23 14:11:23 +0530

    adding python day 1 about data types

---
+++ day02/datatypes.py
@@ -0,0 +1,68 @@
+
+# tuple
+# string and tuple are immutable in python and js not in other lang
+# Why these are immutable ?
+
+num1= 26
+print(type(num1))
+print(id(num1))
+num2 = 26
+print (id(num2))
+print("varible used to refer the value only not to store")
+
+str1 = "hi"
+print(type(str1))
+print(id(str1))
+str2 = 'hi'
+print (id(str2))
+
+print('number of different varibles with same values have a same location/id.. if we want to change a particular varible then all the varibles should change so to this bug strings in python are immutable ')
+
+
+name = "str"
+name = "str"
+name = "str"
+name = "str"
+name = "str"
+print(name);
+print(id(str))
+name = "str"
+name = "str"
+name = "str1"
+print(name)
+print(id(name))
+
+print('so we cannt change particular index value of the string onl ')
+print('--------')
+
+# tuple immutable
+tup1 = (1, 'hi',True, [1,"manisha"],"string")
+print(type(tup1))
+print(tup1[1]) 
+# tup1[1]= 'welcome';
+# tuple is immutable;
+print(tup1[1]) 
+print(tup1);
+tup1 += (1)
+print(tup1)
+
+# difference blt tuple and list... important
+
+# adv of tuple
+# 1. not to make any chance by user
+# it will dublicate the code and always to make change
+
+# range - 
+# range(0,10)
+r1=range(0,10)
+print(list(r1))
+
+# slice
+print()
+
+# steps to skip
+print(r1[2: 10: 2])
+
+
+
+
